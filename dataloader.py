11c96954afbb5ef00d003ef466e9da9622a35a1e
Peter Siemen <peter.siemen@googlemail.com>
2019-06-06 09:56:00 +0200

    load and scale

---
+++ dataloader.py
@@ -0,0 +1,39 @@
+import torch
+from torchvision import datasets
+from torchvision import transforms
+
+
+def get_dataloader(batch_size, image_size, data_dir='processed_celeba_small/'):
+    """
+    Batch the neural network data using DataLoader
+    :param batch_size: The size of each batch; the number of images in a batch
+    :param img_size: The square size of the image data (x, y)
+    :param data_dir: Directory where image data is located
+    :return: DataLoader with batched data
+    """
+
+    # TODO: Implement function and return a dataloader
+    transform = transforms.ToTensor()
+    dataset = datasets.ImageFolder(data_dir, transform)
+
+    dataloader = torch.utils.data.DataLoader(dataset=dataset,
+                                             batch_size=batch_size,
+                                             shuffle=True,
+                                             num_workers=1)
+
+    return dataloader
+
+
+# Define function hyperparameters
+batch_size = 16
+img_size = 32
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
+"""
+# Call your function and get a dataloader
+celeba_train_loader = get_dataloader(batch_size, img_size)
+
+
+batch = next(iter(celeba_train_loader))
+print(batch)
